1e87e1d36941beefa89ee5489fc0e99b4b034bff
Liviu Ene <eneliviu@gmail.com>
2024-07-10 17:33:29 +0200

    Add additional functions and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -4,7 +4,7 @@
 
 // Instal module in nodejs: npm install -D jest-environment-jsdom
 
-const {game} = require('../game');
+const { game, newGame, showScore } = require('../game');
 
 beforeAll(() => {
     let fs = require('fs');
@@ -18,6 +18,49 @@ describe('game object contains correct keys', () => {
     test('score key exists', () => {
         expect('score' in game).toBe(true);
     });
+
+    test('currentGame key exist', ()=>{
+        expect('currentGame' in game).toBe(true);
+    })
+
+    test('playerMoves key exists', () => {
+        expect('playerMoves' in game).toBe(true);
+    });
+
+    test('choices key exists', () =>{
+        expect('choices' in game).toBe(true);
+    });
+
+    test('choices containg correct IDs', () => {
+        expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
+    });
+
+
     
 });
+
+describe('newGame works correctly', () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ['button1', 'button2'];
+        game.currentGame = ['button1', 'button2'];
+        document.getElementById('score').innerText = '42';
+        newGame();
+    });
+    test('should set game score to zero', () => {
+        expect(game.score).toEqual(0);
+    });
+
+    test('should clear the computer sequence array', () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+
+    test('should clear the player moves array', () =>{
+        expect(game.playerMoves.length).toBe(0);
+    });
+
+    test('should display 0 for the element with ID of score', () => {
+        expect(document.getElementById('score').innerText).toEqual(0);
+    });
+})
 
